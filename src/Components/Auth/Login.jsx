afc5611c606a780652ad86378c85fb90d1426285
Alvaro Martinez <alvaromfdv@gmail.com>
2024-03-26 10:59:37 +0100

    isLoggedIn fixed

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../utils/auth.context"; // Importa el contexto de autenticación
+import { Link, useNavigate } from "react-router-dom"; // Importa useNavigate desde react-router-dom
+import { loginService } from "../../utils/auth.services.js"; // Importa el servicio de inicio de sesión
 
-const Login = () => {
+const LoginPage = () => {
+  const { authenticateUser } = useContext(AuthContext); // Usa el contexto de autenticación
+  const navigate = useNavigate(); // Utiliza useNavigate para manejar la navegación
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      // Llama al servicio de inicio de sesión para obtener el token de autenticación
+      const { authToken } = await loginService(credentials);
+      // Guarda el token de autenticación en localStorage
+      localStorage.setItem("authToken", authToken);
+      // Llama a la función authenticateUser para establecer el estado de autenticación en la aplicación
+      await authenticateUser();
+      // Redirige al usuario a la página principal después de iniciar sesión correctamente
+      navigate("/");
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="auth--container">
+      <div className="form--wrapper">
+        <form onSubmit={handleLogin}>
+          <div className="field--wrapper">
+            <label>
+              Email:
+              <input
+                required
+                type="email"
+                name="email"
+                placeholder="Enter your email..."
+                value={credentials.email}
+                onChange={handleInputChange}
+                autoComplete="email"
+              />
+            </label>
+          </div>
 
-export default Login
+          <div className="field--wrapper">
+            <label>
+              Password:
+              <input
+                required
+                type="password"
+                name="password"
+                placeholder="Enter password..."
+                value={credentials.password}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+
+          <div className="field--wrapper">
+            <input
+              type="submit"
+              value="Login"
+              className="btn btn--lg btn--main"
+            />
+          </div>
+        </form>
+
+        <p>
+          Don't have an account? Register <Link to="/register">here</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { Form, Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import navlogoImg from "../Assets/nav-logo-Image.svg";
 import navlogoLAGRIMA from "../Assets/nav-logo-LAGRIMA.svg";
 import navlogoAdmin from "../Assets/nav-logo-Admin.svg";
 import LoginIcon from "../Assets/login.svg";
 import LogoutIcon from "../Assets/logout.svg";
+import { AuthContext } from "../../utils/auth.context"; // Importa el contexto de autenticación
 
 const Navbar = () => {
+  const { user, isLoggedIn, authenticateUser } = useContext(AuthContext); // Obtiene el usuario y el estado de autenticación del contexto de autenticación
+  console.log(user);
+  console.log(isLoggedIn);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      // Elimina el token del almacenamiento local
+      localStorage.removeItem("authToken");
+      // Llama a la función de autenticación con un objeto vacío para cerrar la sesión
+      await authenticateUser({});
+      // Redirige al usuario a la página de inicio de sesión
+      navigate("/login");
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
   return (
     <div className="navbar">
       <Link to="/">
@@ -27,7 +46,22 @@ const Navbar = () => {
           </div>
         </div>
       </Link>
-      <img src={LoginIcon} className="nav-profile" alt="" />
+      {isLoggedIn ? ( // Si hay un usuario autenticado, muestra su nombre y botón de logout
+        <div className="nav-profile">
+          <p>Welcome {user.name}</p>
+          <img
+            src={LogoutIcon}
+            className="nav-profile--icon"
+            alt="Logout"
+            onClick={handleLogout}
+          />
+        </div>
+      ) : (
+        // Si no hay usuario autenticado, muestra el botón de login
+        <Link to="/login">
+          <img src={LoginIcon} className="nav-profile" alt="Login" />
+        </Link>
+      )}
     </div>
   );
 };
